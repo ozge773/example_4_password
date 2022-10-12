83c75e8f0ee24759c323bdf8efdabd2e22495b5c
Ozge Fedai <ozgefedai@gmail.com>
2022-10-11 23:31:57 -0400

    Update example_4.sh

--- example_4.sh
+++ example_4.sh
@@ -6,7 +6,7 @@ wget -O get_info.sh -P down_ex_4 http://localhost:4083/get_info.sh
 chmod +x get_info.sh
 gzip get_info.sh
 
-scp info.txt basanta@172.22.168.180:~/Desktop/scripts
+scp get_info.sh basanta@172.22.168.180:~/Desktop/scripts
 
-rm info.txt
+
 rm get_info.sh
